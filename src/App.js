d5eb87e2e5997dc016618b61ee2fa1f5d536f82e
KavinduPiyumal <piyumal27100@gmail.com>
2022-01-24 12:32:54 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@
 
 function App() {
   return (
-    <div className="App">
-        <h1>Hello</h1>
+    <div className="App container mt-5">
+        <h1 className="text-primary">
+          <i className="bi bi-diagram-2-fill"></i>Repos
+        </h1>
     </div>
   );
 }
